10f22ea9d2ddfa688342350200044427bd30e991
nataliaocmo <nataliaocmo@unisabana.edu.co>
2025-01-25 16:15:14 -0500

    admin home

--- src/components/TopImg.jsx
+++ src/components/TopImg.jsx
@@ -1,6 +1,7 @@
 import foto3 from '../assets/foto3.jpg'; 
 import foto4 from '../assets/foto4.jpg'; 
 import foto5 from '../assets/foto5.jpg';
+import foto6 from '../assets/foto6.jpg';
 export default function TopImg({ number }) {
   const getImage = () => {
     switch (number) {
@@ -10,6 +11,8 @@ export default function TopImg({ number }) {
         return foto4; 
       case 5:
       return foto5;
+      case 6:
+      return foto6;
       default:
         return foto3;
     }
--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -4,14 +4,54 @@ import TopImg from '../components/TopImg';
 import Header from '../components/Header';
 
 export default function AdminHome() {
+  const usuarios= [
+    { id: 1, nombre: 'User 1', apellido: 'Descripción 1', telefono: '877689', correo: 'hola@gmail.com' },
+    { id: 2, nombre: 'User 1', apellido: 'Descripción 1', telefono: '877689', correo: 'hola@gmail.com' },
+    { id: 3, nombre: 'User 1', apellido: 'Descripción 1', telefono: '877689', correo: 'hola@gmail.com' },
+    { id: 4, nombre: 'User 1', apellido: 'Descripción 1', telefono: '877689', correo: 'hola@gmail.com' },
+  ];
   return (
     <div className='Admin-background'>
       <Header type="admin"/>
-      <TopImg number={3}/>
+      <TopImg number={6}/>
       <div className='Admin-card'>
-        <h2>Administrar miembros</h2>
-        <h2></h2>
-
+        <div className='Admin-card-header'>
+        <h2 className='Admin-title'>Administrar miembros</h2>
+        <div>
+        <button className='Add-button'>
+          Crear nuevo miembro
+        </button>
+        </div>
+      
+        <table className="Users-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Teléfono</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usuarios.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.nombre}</td>
+              <td>{user.apellido}</td>
+              <td>{user.telefono}</td>
+              <td>{user.correo}</td>
+              <td>
+                <button onClick={() => handleEditUser(user.id)}>Editar</button>
+                <button onClick={() => handleDeleteUser(user.id)}>Borrar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+      
       </div>
     </div>
   );
