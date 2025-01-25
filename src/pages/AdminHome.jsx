0681d7285939d239d7bca07d09250b2c51c584cc
sebas3261 <sebassanchez3261@gmail.com>
2025-01-25 13:00:44 -0500

    fix

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import "../css/AdminHome.css";
-import Header from '../components/header';
 import TopImg from '../components/TopImg';
+import Header from '../components/Header';
 
 export default function AdminHome() {
   return (
     <div className='Admin-background'>
-      <Header type="admin" />
+      <Header type="admin"/>
       <TopImg number={3}/>
       <div className='Admin-card'>
         <h2>Administrar miembros</h2>
