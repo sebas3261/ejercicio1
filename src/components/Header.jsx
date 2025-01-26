33bdc86eba8833997f1082b316c7e8638e3b3080
nataliaocmo <nataliaocmo@unisabana.edu.co>
2025-01-25 20:01:40 -0500

    logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./header.css";
 import { NavLink } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router";
 export default function Header({ type }) {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout =  () => {
+    logout();
+    navigate("/");
+
+    
+  }
   if (type == "admin") {
     return (
       <div className="header-container">
@@ -10,22 +20,31 @@ export default function Header({ type }) {
             <p>Inicio</p>
           </NavLink>
           <NavLink to={"/entrenosAdmin"} className={"navlink"}>
-          <p>Entrenamiento</p>
+            <p>Entrenamiento</p>
           </NavLink>
           <NavLink to={"/torneosAdmin"} className={"navlink"}>
-          <p>Torneos</p>
+            <p>Torneos</p>
           </NavLink>
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="40"
-          height="40"
-          fill="currentColor"
-          clasName="bi bi-person"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-        </svg>
+        <div onClick={handleLogout}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            fill="currentColor"
+            class="bi bi-box-arrow-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+            />
+            <path
+              fill-rule="evenodd"
+              d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+            />
+          </svg>
+        </div>
       </div>
     );
   }
@@ -33,28 +52,37 @@ export default function Header({ type }) {
     <div className="header-container">
       <div className="header-content">
         <NavLink to={"/userdashboard"} className={"navlink"}>
-        <p>Inicio</p>
+          <p>Inicio</p>
         </NavLink>
         <NavLink to={"/entrenosUser"} className={"navlink"}>
-        <p>Mis entrenamientos</p>
+          <p>Mis entrenamientos</p>
         </NavLink>
         <NavLink to={"/torneosUser"} className={"navlink"}>
-        <p>Mis torneos</p>
+          <p>Mis torneos</p>
         </NavLink>
         <NavLink to={"/infouser"} className={"navlink"}>
-        <p>Mi Información</p>
+          <p>Mi Información</p>
         </NavLink>
       </div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="40"
-        height="40"
-        fill="currentColor"
-        clasName="bi bi-person"
-        viewBox="0 0 16 16"
-      >
-        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-      </svg>
+      <div onClick={handleLogout}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          fill="currentColor"
+          class="bi bi-box-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
